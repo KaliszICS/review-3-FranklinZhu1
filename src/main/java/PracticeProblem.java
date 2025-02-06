f75db36010c53cc78d5b66c95f7cda8f4d1bb77d
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-02-06 15:37:28 +0000

    Create (untested) solution (the Erlenmeyer flask will not appear)

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,38 +13,63 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String s = in.nextLine();
+		System.out.println(s.charAt(0));
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		boolean b = in.nextBoolean();
+		in.nextLine();
+		System.out.println(!b);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		int n = in.nextInt();
+		in.nextLine();
+		System.out.println(n > 5);
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		double d = in.nextDouble();
+		in.nextLine();
+		System.out.println(-2 <= d && d <= 2);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String s = in.nextLine();
+		System.out.println(s.equals("Hello World"));
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		int a = in.nextInt();
+		in.nextLine();
+		System.out.print("In: ");
+		int b = in.nextInt();
+		in.nextLine();
+		System.out.println(a <= b);
 	}
 
 	public static void q7() {
-		//Write question 7 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		double a = in.nextInt();
+		in.nextLine();
+		System.out.print("In: ");
+		double b = in.nextInt();
+		in.nextLine();
+		System.out.println(a > b);
 	}
 
 }
